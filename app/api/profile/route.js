f0ec912479649a8d218f3404186c99b17e02b7d3
mouy2002 <chalinee.f@ku.th>
2024-03-17 15:02:10 +0700

    Auth

---
+++ app/api/profile/route.js
@@ -0,0 +1,10 @@
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from '../auth/[...nextauth]/route'
+
+export async function GET(request) {
+  const session = await getServerSession(authOptions)
+
+  return Response.json({
+    message: 'test'
+  })
+}
